12b96e3804e62be1f2bcacc9803aee65573b2563
31418211 <31418211@099065-P31S202.macklabs.br>
2016-03-28 08:58:16 -0300

    foi add :  public SaldoInsuficienteException() {         super("Saldo insuficiente, colocar mais dinheiro na maquina para finalizar a compra do bilhete");     }
    na classe SaldoInsuficienteException

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,7 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+     public SaldoInsuficienteException() {
+        super("Saldo insuficiente, colocar mais dinheiro na maquina para finalizar a compra do bilhete");
+    }
 }
